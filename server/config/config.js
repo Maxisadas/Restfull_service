d33eb46c5838b72ee3f0cd7b4ef5aab444d94c5e
Maxisadas <33811462+Maxisadas@users.noreply.github.com>
2019-10-26 19:15:51 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,21 @@
 process.env.PORT = process.env.PORT || 3000;
+
+//ENTORNO
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// BASE DE DATOS
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://localhost/cafe';
+} else {
+
+    urlDB = "mongodb+srv://Maxisadas:SWJ7PfqLvOf5F9aR@cluster0-58snd.mongodb.net/cafe";
+
+}
+
+process.env.urlDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 require('./config/config');
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -9,26 +11,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/routeUsuario'))
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
+mongoose.connect(process.env.urlDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
-    res.json(body);
-});
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({
-
-        id
-    })
-});
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-});
 app.listen(process.env.PORT, () => {
 
     console.log('Esuchando puerto:', process.env.PORT);
